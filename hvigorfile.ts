013518f9b40626f16d1bffb5753f382cb33357dd
AbnerMing <gw00261583@gwm.cn>
2025-08-19 11:18:40 +0800

    提交1.0.5版本

--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,9 +1,9 @@
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
-// 导入接口
-import { HvigorNode, HvigorPlugin } from '@ohos/hvigor';
-
+import { abnerRouter } from 'ohos-router/router-plugin';
 export default {
   system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []         /* Custom plugin to extend the functionality of Hvigor. */
+  plugins: [
+    abnerRouter()
+  ]         /* Custom plugin to extend the functionality of Hvigor. */
 }
 
